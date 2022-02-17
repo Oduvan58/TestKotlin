39b02567cc15900f4e5268c26f6ec420cf2a870e
Oduvan58 <yancheuskipavel@gmail.com>
2022-02-17 21:05:20 +0300

    Add button with click

--- app/src/main/java/by/geekbrains/testkotlin/MainActivity.kt
+++ app/src/main/java/by/geekbrains/testkotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package by.geekbrains.testkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import by.geekbrains.testkotlin.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<MaterialButton>(R.id.test_button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello my friend!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
