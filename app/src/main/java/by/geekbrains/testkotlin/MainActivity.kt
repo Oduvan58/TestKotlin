def50c080b89690a5ad547cc2f23f6f5733235d8
Oduvan58 <yancheuskipavel@gmail.com>
2022-02-18 17:59:34 +0300

    Add examples of cycles

--- app/src/main/java/by/geekbrains/testkotlin/MainActivity.kt
+++ app/src/main/java/by/geekbrains/testkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package by.geekbrains.testkotlin
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
@@ -11,11 +12,19 @@ class MainActivity : AppCompatActivity() {
     lateinit var firstNameTextView: TextView
     lateinit var lastNameTextView: TextView
 
+    private lateinit var friendList: List<Friend>
+
     private val fullName = Friend("Pavel", "Yancheuski")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFriendsList()
+
+        firstCycle()
+        secondCycle()
+        thirdCycle()
 
         initViews()
         helloButton.setOnClickListener {
@@ -24,9 +33,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun initViews() {
+    private fun initViews() {
         firstNameTextView = findViewById(R.id.first_name_text_view)
         lastNameTextView = findViewById(R.id.last_name_text_view)
         helloButton = findViewById(R.id.test_button)
+    }
+
+    private fun initFriendsList() {
+        friendList = listOf(Friend("James", "Bond"))
+    }
+
+    private fun firstCycle() {
+        for (friend in friendList) {
+            Log.i("TAG", friend.toString())
+        }
+    }
+
+    private fun secondCycle() {
+        for (i in 1..10) {
+            Log.i("TAG", "Hello")
+        }
+    }
+
+    private fun thirdCycle() {
+        for (i in 10 downTo 1 step 2) {
+            Log.i("TAG", "Friend")
+        }
     }
 }
